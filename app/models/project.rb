9cfc3769badcd05c88256bfa3ae2a8a952cdba06
Jared Cosulich <jared@irrationaldesign.com>
2013-10-21 15:23:18 -0700

    working on projects page

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,7 +5,7 @@ class Project < ActiveRecord::Base
   has_many :materials
   
   has_attached_file :photo, 
-    :styles => { :rect => "300x200#", :medium => "300x300>", :thumb => "100x100>" }, 
+    :styles => { rect: '300x200#', square: '150x150#', medium: '300x300>', thumb: '100x100>' }, 
     :default_url => "/images/:style/missing.png"
 
 end
