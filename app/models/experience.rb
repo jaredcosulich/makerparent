7fe1c4fa42aa94caf61fcca82cb078b3cef2890b
Jared Cosulich <jared@irrationaldesign.com>
2013-12-04 11:50:31 -0800

    working on experience reports

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,5 +1,13 @@
 class Experience < ActiveRecord::Base
 
   belongs_to :project
+  
+  
+  def simple_description(long=false)
+    return '' unless simple
+    descriptions = long ? Project::LONG_ESTIMATED_SIMPLE_DESCRIPTIONS : Project::ESTIMATED_SIMPLE_DESCRIPTIONS
+    descriptions[simple]
+  end  
+  
 
 end
