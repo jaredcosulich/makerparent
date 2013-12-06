f4ae579614f761bf29495297943b7d73d205ca2f
Jared Cosulich <jared@irrationaldesign.com>
2013-12-06 13:56:43 -0800

    adding in admin user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,12 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-   has_attached_file :photo, 
-     :styles => { rect: '300x200#', thumb: '100x100>' }, 
-     :default_url => "/images/:style/missing.png"
+  has_attached_file :photo, 
+    :styles => { rect: '300x200#', thumb: '100x100>' }, 
+    :default_url => "/images/:style/missing.png"
+
+  def admin?
+    return [1].index(id).present?
+  end
 
 end
