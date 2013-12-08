c87c5326464da285e0b0dde0369765665f07d8bc
Jared Cosulich <jared@irrationaldesign.com>
2013-12-08 09:02:52 -0800

    adding in stats about projects

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,8 +2,9 @@ class WelcomeController < ApplicationController
   def index
     # @hide_navbar = true
     
-    @recent = Project.order('created_at desc').limit(3)
-    @simple = Project.order('average_simple asc, experiences_count desc').limit(3)
-    @popular = Project.where.not(id: (@recent + @simple).map(&:id)).order('experiences_count desc').limit(3)
+    with_photo = Project.where('photo_content_type is not null')
+    @recent = with_photo.order('created_at desc').limit(3)
+    @simple = with_photo.order('average_simple asc, experiences_count desc').limit(3)
+    @popular = with_photo.where.not(id: (@recent + @simple).map(&:id)).order('experiences_count desc').limit(3)
   end
 end
