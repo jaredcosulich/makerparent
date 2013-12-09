2e3211950f6d8f63766cd98aa8a72e4e14a0b096
Jared Cosulich <jared@irrationaldesign.com>
2013-12-08 16:52:45 -0800

    track experiences

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -4,11 +4,20 @@ class Experience < ActiveRecord::Base
   belongs_to :project
   has_many :experience_photos, dependent: :destroy
   
+  after_create :update_project
+  after_destroy :update_project
+  
   def simple_description(long=false)
     return '' unless simple
     descriptions = long ? Project::LONG_ESTIMATED_SIMPLE_DESCRIPTIONS : Project::ESTIMATED_SIMPLE_DESCRIPTIONS
     descriptions[simple]
   end  
+  
+  private
+    
+    def update_project
+      project.update_attributes(experiences_count: Experience.where(project_id: self.project_id).count)
+    end
   
 
 end
