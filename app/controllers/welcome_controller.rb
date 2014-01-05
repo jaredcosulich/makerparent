c982ec976f39e6e4bd34c7a8b47186bc9e6cddc9
Jared Cosulich <jared@irrationaldesign.com>
2014-01-05 09:43:25 -0800

    don't show duplicate projects on front page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,8 +3,8 @@ class WelcomeController < ApplicationController
     # @hide_navbar = true
     
     with_photo = Project.where('photo_content_type is not null')
-    @recent = with_photo.order('created_at desc').limit(3)
     @simple = with_photo.order('average_simple asc, experiences_count desc').limit(3)
+    @recent = with_photo.where.not(id: @simple.map(&:id)).order('created_at desc').limit(3)
     @popular = with_photo.where.not(id: (@recent + @simple).map(&:id)).order('experiences_count desc, bookmarks_count desc').limit(3)
   end
 end
