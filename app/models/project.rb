140da4cde5954b9cd523555ecdedb51bc0c03a3f
Jared Cosulich <jared@irrationaldesign.com>
2014-02-19 10:05:41 -0800

    remove wide thumbnail

--- app/models/project.rb
+++ app/models/project.rb
@@ -8,9 +8,13 @@ class Project < ActiveRecord::Base
   has_many :bookmarks, counter_cache: true, dependent: :destroy
   
   has_attached_file :photo, 
-    :styles => { rect: '300x200#', square: '150x150#', medium: '300x300>', wide: '540x300>', thumb: '100x100>' },
-    :convert_options => {:wide => "-gravity center -extent 540x300"}, 
+    :styles => { rect: '300x200#', square: '150x150#', medium: '300x300>', thumb: '100x100>' },
     :default_url => "/images/:style/missing.png"
+
+  # has_attached_file :photo, 
+  #   :styles => { rect: '300x200#', square: '150x150#', medium: '300x300>', wide: '540x300>', thumb: '100x100>' },
+  #   :convert_options => {:wide => "-gravity center -extent 540x300"}, 
+  #   :default_url => "/images/:style/missing.png"
 
   before_create :set_defaults
   before_save :calculate_averages
@@ -61,7 +65,7 @@ class Project < ActiveRecord::Base
     """
     
     client.photo("makerparent.tumblr.com", {
-      source: photo.url(:wide),
+      source: photo.url(:medium),
       link: "http://www.makerparent.com/projects/#{id}",
       caption: caption
     })
