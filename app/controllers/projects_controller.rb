4ad250ab9d5a8b2b31dee5e82061b80d234bb8a6
Jared Cosulich <jared@irrationaldesign.com>
2014-02-19 17:36:42 -0800

    better popular order

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -17,7 +17,7 @@ class ProjectsController < ApplicationController
     [:sort, :sort2].each do |param|
       if params.include?(param)
         if params[param] == 'popular'
-          order << 'experiences_count desc'
+          order << '(experiences_count - 1) desc'
           order << 'bookmarks_count desc'
         elsif params[param] == 'simple'
           order << 'average_simple asc'
