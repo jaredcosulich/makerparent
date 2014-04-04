4723d630db7b01fe3413647ef1b8eb3d0854c8b3
Jared Cosulich <jared@irrationaldesign.com>
2014-04-04 10:10:17 -0700

    removing s3 data

--- app/models/experience_photo.rb
+++ app/models/experience_photo.rb
@@ -8,17 +8,7 @@ class ExperiencePhoto < ActiveRecord::Base
     Cloudinary::Uploader.destroy(self.cloudinary_id, invalidate: true)
   end
   
-  # def delete_s3
-  #   service = S3::Service.new(:access_key_id => "AKIAJBWPH3QHEBOS6VEA",
-  #                             :secret_access_key => "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")
-  # 
-  #   ['original', 'large', 'small'].each do |size|
-  #     bucket = service.buckets.find("simpleneat-#{size}")
-  #     object = bucket.objects.find("#{s3_id}.jpg")
-  #     object.destroy
-  #   end
-  # end
-  
+
   def delete_hosted
     delete_cloudinary# if cloudify_deleted_at.nil?
     #delete_s3 unless s3_added_at.nil?
